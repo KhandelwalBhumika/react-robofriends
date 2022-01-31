7f71350c14d0c7c17b2cf02c6cfb97cb52981feb
Bhumika khandelwal <94340974+KhandelwalBhumika@users.noreply.github.com>
2022-01-31 16:39:58 +0530

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,60 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Bhumika Gupta',
+        username: 'Bhumi',
+        email: 'Bhumika.gupta@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Manu Jain',
+        username: 'Aniket',
+        email: 'aniket99@gmail.com'
+    },  {
+        id: 3,
+        name: 'Alex George',
+        username: 'alex',
+        email: 'georgealex@hotmail.com'
+    },  {
+        id: 4,
+        name: 'Olivia Cyrus',
+        username: 'Oli',
+        email: 'Olivia 88@gmail.com'
+    },  
+    {
+        id: 5,
+        name: 'Mike Tyson',
+        username: 'Mike',
+        email: 'tyson_mike@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Dhriti Sen',
+        username: 'Dhri',
+        email: 'Ddhriti01@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Siri Ale',
+        username: 'Siri',
+        email: 'ale..siri@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Monica Murphy',
+        username: 'Monica',
+        email: 'Monicaaa@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Rachel Gonsalves',
+        username: 'Rachel',
+        email: 'r_gonsalves@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Shaun Kyle',
+        username: 'Shaun',
+        email: 'shaun.kyle@hotmail.com'
+    }
+]
